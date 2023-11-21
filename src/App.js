d30d20a1e4596f8533f07a9cc6c117426feb3321
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-11-21 11:50:41 +0100

    add tailwind config colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <h1>Test Creation</h1>
+      <h1 className="text-accentPurple">Test Creation</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accentGreen: "#E7FAAA",
+        accentPurple: "#B89BDF",
+        lightColor: "#FFFFFF",
+        darkColor: "#232323",
+        darkGray: "#454545",
+        lightGray: "#696969",
+      },
+    },
   },
   plugins: [],
-}
-
+};
