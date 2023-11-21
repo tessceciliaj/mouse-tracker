e47b1b86de737adc02c6216284648b3c411715cd
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-11-21 14:00:24 +0100

    Add header component, header tests with titles and count state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+import Header from "../src/components/Header";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <h1 className="text-accentPurple">Test Creation</h1>
+      <Header title="Mouse Tracker" initialCount={count} />
     </div>
   );
 }
