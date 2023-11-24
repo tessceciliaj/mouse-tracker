2a1d9249e6c70dab7c9e8462c4bad04a5df26394
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-11-24 09:53:52 +0100

    Add more card test

--- src/tests/Card.test.js
+++ src/tests/Card.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Card from '../components/Card'
+import OneClickResult from '../components/OneClickResult'
 import App from '../App'
 
 describe('card has its expected elements', () => {
@@ -35,4 +36,43 @@ describe('card has its expected elements', () => {
 
     expect(resultEl).toBeInTheDocument()
   })
+
+  test('clicking the "Get a Star" button displays an image', () => {
+    render(<Card />);
+  
+    const button = screen.getByRole('button', { name: /get a star/i });
+    fireEvent.click(button);
+  
+    const image = screen.getByAltText('Star');
+  
+    expect(image).toBeInTheDocument();
+  });
+
+
+  test('clicking the button triggers the handleShowImage function', () => {
+    const handleShowImageMock = jest.fn();
+  
+    render(<Card />);
+  
+    const button = screen.getByRole('button', { name: /get a star/i });
+    button.onclick = handleShowImageMock;
+    fireEvent.click(button);
+  
+    expect(handleShowImageMock).toHaveBeenCalled();
+  });
+  
+  test('OneClickResult is rendered within Card', () => {
+    render(
+      <Card>
+        <OneClickResult />
+      </Card>
+    );
+
+    const oneClickResultElement = screen.getByTestId('oneclickresult');
+    const cardElement = screen.getByTestId('card');
+
+    expect(cardElement).toContainElement(oneClickResultElement);
+  });
+
+  
 })
